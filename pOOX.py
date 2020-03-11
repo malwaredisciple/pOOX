628dcc65e8453073112526a0d2c12a8a897cc9c0
malwaredisciple <61957366+malwaredisciple@users.noreply.github.com>
2020-03-10 22:32:48 -0400

    Update pOOX.py

--- pOOX.py
+++ pOOX.py
@@ -4,17 +4,32 @@ of malware samples of the OOXML file format.
 """
 
 from zipfile import ZipFile
+from tree import DisplayablePath
 from pathlib import Path
 import os
 import re
 import xml.dom.minidom
+import hashlib
 
 
 class OOXMLparser:
+    """
+    docstring
+    """
+    TYPE_OLE_OBJ = 'http://schemas.openxmlformats.org/officeDocument/2006/relationships/oleObject'
+    TYPE_FRAME = 'http://schemas.openxmlformats.org/officeDocument/2006/relationships/frame'
+    TYPE_TEMPLATE = 'http://schemas.openxmlformats.org/officeDocument/2006/relationships/attachedTemplate'
+    TYPE_VBA_PROJ = 'http://schemas.microsoft.com/office/2006/relationships/vbaProject'
+    TYPE_EXTERNAL = 'External'
+
     def __init__(self, file_path):
         self.file_path = file_path
         self.file_name = re.split(r'(/|\\)', file_path)[-1]
-        self.new_dir = '{}_unzipped'.format(self.file_name)
+        self.new_dir = '{}_pOOX'.format(self.file_name)
+        self.file_data = open(file_path, 'rb').read()
+        self.md5 = hashlib.md5(self.file_data).hexdigest()
+        self.sha1 = hashlib.sha1(self.file_data).hexdigest()
+        self.sha256 = hashlib.sha256(self.file_data).hexdigest()
         self._is_xls = False
         self._is_doc = False
         self._is_ppt = False
@@ -22,12 +37,16 @@ class OOXMLparser:
         self.doc_dir = None
         self._has_embeddings = False
         self._has_remote_template = False
+        self._has_remote_frame = False
+        self._has_macros = False
+        self.vba_bin = None
+        self._has_ole = False
+        self._has_dde = False
         self._remote_template = None
-
-    def print_tree(self):
-        paths = DisplayablePath.make_tree(Path(self.new_dir))
-        for path in paths:
-            print(path.displayable())
+        self.remote_frame = None
+        self.ole_object = None
+        self.embeddings = None
+        self.docs_rels = set()
 
     def unzip(self):
         with ZipFile(self.file_path) as zip:
@@ -59,119 +78,104 @@ class OOXMLparser:
             self.main_xml = self.get_data('{}/presentation.xml'.format(self.doc_dir))
 
     def set_embeddings(self):
-        if 'embeddings' in os.listdir(self.new_dir):
+        if 'embeddings' in os.listdir(self.new_dir) or 'embeddings' in os.listdir(self.doc_dir):
             self._has_embeddings = True
+            self.embeddings = os.listdir('{}/embeddings'.format(self.doc_dir))
 
     @staticmethod
     def get_data(path):
         return open(path, 'r').read()
 
-    def check_remote_template(self):
-        rels_xml = xml.dom.minidom.parse('{}/_rels/settings.xml.rels'.format(self.doc_dir))
-        rels = rels_xml.getElementsByTagName('Relationship')
-        for rel in rels:
-            if (rel.getAttribute('Type') ==
-                    'http://schemas.openxmlformats.org/officeDocument/2006/relationships/attachedTemplate'
-                    and rel.getAttribute('TargetMode') == 'External'):
-                self._has_remote_template = True
-                self._remote_template = rel.getAttribute('Target')
+    def print_report(self):
+        self.print_art()
+        self.print_meta()
+        self.print_tree()
+        self.print_analysis()
+        print('')
+
+    def print_analysis(self):
+        print('\nAnalysis:')
+        if self._has_macros:
+            print('[+] contains VBA macros -> {}'.format(self.vba_bin))
+        if self._has_remote_template:
+            print('[+] remote template injection -> {}'.format(self._remote_template))
+        if self._has_remote_frame:
+            print('[+] remote frame injection -> {}'.format(self.remote_frame))
+        if self._has_embeddings:
+            print('[+] contains embedded files -> {}'.format(self.embeddings))
+        if self._has_ole:
+            print('[+] contains oleobject -> {}'.format(self.ole_object))
+
+    def print_tree(self):
+        print('\nTree View of De-archived OOXML:\n{}'.format('-' * 31))
+        paths = DisplayablePath.make_tree(Path(self.new_dir))
+        for path in paths:
+            print(path.displayable())
+
+    def print_meta(self):
+        print('\nMetadata: \n{}'.format('-' * 9))
+        print('Sample: {}'.format(self.file_path))
+        print('MD5: {}'.format(self.md5))
+        print('SHA1: {}'.format(self.sha1))
+        print('SHA256: {}'.format(self.sha256))
+
+    @staticmethod
+    def print_art():
+        print('{}{}{}'.format('+', '-' * 58, '+'))
+        print('|{}pOOX - Parse OOXML Samples{}|'.format(' ' * 17, ' ' * 15))
+        print('+{}+'.format('-' * 58))
+        print('|{}author: @malwaredisciple{}|'.format(' ' * 18, ' ' * 16))
+        print('+{}+'.format('-' * 58))
+        print('|{}https://github.com/malwaredisciple/pOOX{}|'.format(' ' * 11, ' ' * 8))
+        print('+{}+'.format('-' * 58))
+        print('\n+{}+\n|pOOX Report|\n+{}+'.format('-' * 11, '-' * 11))
+
+    def set_doc_rels(self):
+        if self._is_xls and 'sheet1.xml.rels' in os.listdir('{}/worksheets/_rels'.format(self.doc_dir)):
+            self.docs_rels.add('{}/worksheets/_rels/sheet1.xml.rels'.format(self.doc_dir))
+        if self._is_xls and 'workbook.xml.rels' in os.listdir('{}/_rels'.format(self.doc_dir)):
+            self.docs_rels.add('{}/_rels/workbook.xml.rels'.format(self.doc_dir))
+        if self._is_doc and 'settings.xml.rels' in os.listdir('{}/_rels'.format(self.doc_dir)):
+            self.docs_rels.add('{}/_rels/settings.xml.rels'.format(self.doc_dir))
+        if self._is_doc and 'document.xml.rels' in os.listdir('{}/_rels'.format(self.doc_dir)):
+            self.docs_rels.add('{}/_rels/document.xml.rels'.format(self.doc_dir))
+        if self._is_doc and 'webSettings.xml.rels' in os.listdir('{}/_rels'.format(self.doc_dir)):
+            self.docs_rels.add('{}/_rels/webSettings.xml.rels'.format(self.doc_dir))
+
+    def parse_rels(self, doc_rels):
+        for rels in doc_rels:
+            for rel in xml.dom.minidom.parse(rels).getElementsByTagName('Relationship'):
+                if rel.getAttribute('Type') == self.TYPE_OLE_OBJ:
+                    self._has_ole = True
+                    self.ole_object = rel.getAttribute('Target')
+                elif rel.getAttribute('Type') == self.TYPE_VBA_PROJ:
+                    self._has_macros = True
+                    self.vba_bin = rel.getAttribute('Target')
+                elif (rel.getAttribute('Type') == self.TYPE_FRAME
+                      and rel.getAttribute('TargetMode') == self.TYPE_EXTERNAL):
+                    self._has_remote_frame = True
+                    self.remote_frame = rel.getAttribute('Target')
+                elif (rel.getAttribute('Type') == self.TYPE_TEMPLATE
+                      and rel.getAttribute('TargetMode') == self.TYPE_EXTERNAL):
+                    self._has_remote_frame = True
+                    self.remote_frame = rel.getAttribute('Target')
 
     def start(self):
         try:
             os.mkdir(self.new_dir)
         except:
-            print('[-] Directory -> {}/ already exists!'.format(self.new_dir))
-            return
+            pass
         self.unzip()
-        self.print_tree()
         self.set_type()
         self.set_doc_dir()
-        self.get_main_xml_data()
         self.set_embeddings()
-        self.check_remote_template()
+        self.set_doc_rels()
+        self.parse_rels(self.docs_rels)
+        self.print_report()
         return 1
 
 
-"""
-copypasta - https://stackoverflow.com/questions/9727673/list-directory-tree-structure-in-python
-"""
-class DisplayablePath(object):
-    display_filename_prefix_middle = '├──'
-    display_filename_prefix_last = '└──'
-    display_parent_prefix_middle = '    '
-    display_parent_prefix_last = '│   '
-
-    def __init__(self, path, parent_path, is_last):
-        self.path = Path(str(path))
-        self.parent = parent_path
-        self.is_last = is_last
-        if self.parent:
-            self.depth = self.parent.depth + 1
-        else:
-            self.depth = 0
-
-    @property
-    def displayname(self):
-        if self.path.is_dir():
-            return self.path.name + '/'
-        return self.path.name
-
-    @classmethod
-    def make_tree(cls, root, parent=None, is_last=False, criteria=None):
-        root = Path(str(root))
-        criteria = criteria or cls._default_criteria
-
-        displayable_root = cls(root, parent, is_last)
-        yield displayable_root
-
-        children = sorted(list(path
-                               for path in root.iterdir()
-                               if criteria(path)),
-                          key=lambda s: str(s).lower())
-        count = 1
-        for path in children:
-            is_last = count == len(children)
-            if path.is_dir():
-                yield from cls.make_tree(path,
-                                         parent=displayable_root,
-                                         is_last=is_last,
-                                         criteria=criteria)
-            else:
-                yield cls(path, displayable_root, is_last)
-            count += 1
-
-    @classmethod
-    def _default_criteria(cls, path):
-        return True
-
-    @property
-    def displayname(self):
-        if self.path.is_dir():
-            return self.path.name + '/'
-        return self.path.name
-
-    def displayable(self):
-        if self.parent is None:
-            return self.displayname
-
-        _filename_prefix = (self.display_filename_prefix_last
-                            if self.is_last
-                            else self.display_filename_prefix_middle)
-
-        parts = ['{!s} {!s}'.format(_filename_prefix,
-                                    self.displayname)]
-
-        parent = self.parent
-        while parent and parent.parent is not None:
-            parts.append(self.display_parent_prefix_middle
-                         if parent.is_last
-                         else self.display_parent_prefix_last)
-            parent = parent.parent
-
-        return ''.join(reversed(parts))
-
-
 if __name__ == '__main__':
-    parser = OOXMLparser('/Users/slayer/samples/remote.docx')
+    parser = OOXMLparser('/Users/slayer/samples/PAYMENT_COPY_MT103.xlsx')
     parser.start()
 
